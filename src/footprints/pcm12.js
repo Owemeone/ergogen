cefd75ad38aafafe2f1f7a8f09e0cfef30570dbe
Dave Liefbroer <dave@liefbroer.com>
2022-06-23 06:25:13 +0200

    Updates

--- src/footprints/pcm12.js
+++ src/footprints/pcm12.js
@@ -1,16 +1,23 @@
 module.exports = {
   nets: {
-    from: 'from',
-    to: 'to'
+    from: "from",
+    to: "to",
   },
   params: {
-    class: 'T',
-    reverse: false
+    class: "T",
+    reverse: false,
   },
-  body: p => {
+  body: (p) => {
     const standard = `
-(module Button_Switch_SMD:SW_SPDT_PCM12 (layer F.Cu) (tedit 5A02FC95)
+      (module Button_Switch_SMD:SW_SPDT_PCM12 (layer F.Cu) (tedit 5A02FC95)
         ${p.at /* parametric position */}
+
+        ${"" /* footprint reference */}
+    (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${
+      p.ref_hide
+    } (effects (font (size 1.27 1.27) (thickness 0.15))))
+    (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
+    
   (descr "Ultraminiature Surface Mount Slide Switch, right-angle, https://www.ckswitches.com/media/1424/pcm.pdf")
   (attr smd)
   (fp_line (start -4.4 2.1) (end -4.4 -2.45) (layer F.CrtYd) (width 0.05))
@@ -35,33 +42,37 @@ module.exports = {
     (effects (font (size 1 1) (thickness 0.15)))
   )
 
-      `
+      `;
     function pins(def_neg, def_pos, def_side) {
-          return `
-            ${''/* pins */}
+      return `
+            ${"" /* pins */}
   (pad "" np_thru_hole circle (at -1.5 0.33) (size 0.9 0.9) (drill 0.9) (layers *.Cu *.Mask))
   (pad "" np_thru_hole circle (at 1.5 0.33) (size 0.9 0.9) (drill 0.9) (layers *.Cu *.Mask))
-  (pad 1 smd rect (at ${def_neg}2.25 -1.43) (size 0.7 1.5) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.net.from.str})
+  (pad 1 smd rect (at ${def_neg}2.25 -1.43) (size 0.7 1.5) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${
+        p.net.from.str
+      })
 
-  (pad 2 smd rect (at ${def_pos}0.75 -1.43) (size 0.7 1.5) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.net.to.str})
+  (pad 2 smd rect (at ${def_pos}0.75 -1.43) (size 0.7 1.5) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${
+        p.net.to.str
+      })
   (pad 3 smd rect (at ${def_pos}2.25 -1.43) (size 0.7 1.5) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask))
   (pad "" smd rect (at ${def_neg}3.65 1.43) (size 1 0.8) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask))
   (pad "" smd rect (at ${def_pos}3.65 1.43) (size 1 0.8) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask))
   (pad "" smd rect (at ${def_pos}3.65 -0.78) (size 1 0.8) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask))
   (pad "" smd rect (at ${def_neg}3.65 -0.78) (size 1 0.8) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask))
-          `
+          `;
     }
-    if(p.param.reverse) {
+    if (p.param.reverse) {
       return `
         ${standard}
-        ${pins('-', '', 'F')}
-        ${pins('', '-', 'B')})
-        `
+        ${pins("-", "", "F")}
+        ${pins("", "-", "B")})
+        `;
     } else {
       return `
         ${standard}
-        ${pins('-', '', 'B')})
-        `
+        ${pins("-", "", "B")})
+        `;
     }
-  }
-}
+  },
+};
